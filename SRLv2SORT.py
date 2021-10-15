b828775d2547c2d0b23acd8ed59a0b33cb6dc660
KDKftw <ooo.kadoun@gmail.com>
2021-10-15 17:21:58 +0200

    get src photo, check if displayed, timeout exception

--- SRLv2SORT.py
+++ SRLv2SORT.py
@@ -1,10 +1,11 @@
 from to_import import acceptConsent, URL, URL_stat, caps, URL_groupsearch, closeExponeaBanner
 from to_import_secret import sendEmail
 import time
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 driver = webdriver.Chrome(executable_path=r"C:\Users\KDK\Desktop\Selenium setup\chromedriver94.exe")
 ##sorted by nejlevnejsi
 ##URL_SRL = "https://www.fischer.cz/vysledky-vyhledavani?qf=109_0_50|386_1_0|108_1_0&sortby=PriceTotal&sa=2138|1949|2730&tt=1&to=4312&dd=2021-12-01&rd=2021-12-31&nn=7|8|9&ac1=2&m=5"
--- detail.py
+++ detail.py
@@ -12,10 +12,9 @@ driver = webdriver.Chrome(executable_path=r"C:\Users\KDK\Desktop\Selenium setup\
 
 
 
-URL_no_work = "https://img.fischer.cz/hotels/recko/kreta/plakias/apollo/1162139.jpeg"
-URL_work = "https://cdn.fischer.cz/Images/000409/turecko-turecka-riviera-konakli-eftalia-aqua-resort-0_706x424.jpg"
 
-driver.get("https://www.fischer.cz/turecko/turecka-riviera/side/primasol-hane-family?DS=1024&GIATA=40038&D=627|974|596|712|684|955&HID=8554&MT=5&MMT=5&RT=15&NN=7&RD=2021-11-13&DD=2021-11-06&DP=4312&TO=4305|4309|2682|4308|4312&MNN=7|8|9&TT=1&PID=28963&DPR=EXIM%20TOURS&TTM=1&TOM=4305|4309|2682|4308|4312&DF=2021-11-06|2021-12-07&ERM=0&NNM=7|8|9&ac1=2&kc1=1&ka1=10&ic1=0#/fotky")
+
+driver.get("https://www.fischer.cz/spanelsko/tenerife/playa-la-arena/bahia-flamingo?DS=1&GIATA=6881&D=953|1108|592|611|610|612|590|726|609|621|1009|680|622|669|1086|1194|670|978|594|675|1010|683&HID=114154&MT=2&DI=47&RT=15&NN=7&RD=2021-11-08&DD=2021-11-01&DP=4312&MNN=7|8|9&TT=1&PID=TBAH&DPR=Fischer&TTM=1&DF=2021-11-01|2021-12-02&ERM=0&NNM=7|8|9&ac1=2&kc1=0&ic1=0")
 
 imageDetail = driver.find_element_by_xpath("//*[@id='gallery01Trigger']//img")
 imageDetailSrc = imageDetail.get_attribute("src")
@@ -26,14 +25,15 @@ try:
     driver.set_page_load_timeout(5)
     driver.get(imageDetailSrc)
 except TimeoutException:
-        print("not good")
-
-
+    url = driver.current_url
+    msg = "Problem s fotkou src, TimeoutException " + url
+    sendEmail(msg)
 
 try:
     image = driver.find_element_by_xpath("/html/body/img")
     if image.is_displayed():
         print("its ok")
-
 except NoSuchElementException:
-    print("no such element ex")
+    url = driver.current_url
+    msg = "Problem s fotkou src, NoSuchElementException " + url
+    sendEmail(msg)
