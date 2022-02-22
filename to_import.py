5f3652926bc8ea95eec9ac4bdfa4edce61f235aa
KDKftw <ooo.kadoun@gmail.com>
2022-02-22 20:25:56 +0100

    no message

--- to_import.py
+++ to_import.py
@@ -65,3 +65,17 @@ def closeExponeaBanner(driver):
 
     except NoSuchElementException:
       print("nenasle se exponea banner")
+
+def acceptConsent3(driver):
+  time.sleep(2)
+
+  element = driver.execute_script(
+      """return document.querySelector('#usercentrics-root').shadowRoot.querySelector("button[data-testid='uc-accept-all-button']")""")
+  if element !=0:
+
+      pass
+
+  else:
+      element.click()
+
+
