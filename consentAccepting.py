3b50087347e13eba6c875022efad719710eb3802
KDKftw <ooo.kadoun@gmail.com>
2022-03-14 19:31:23 +0100

    no message

---
+++ consentAccepting.py
@@ -0,0 +1,10 @@
+
+
+
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.wait import WebDriverWait
+from to_import import acceptConsent, URL_groupsearch, setUp, tearDown
+import unittest
+driver = webdriver.Chrome(ChromeDriverManager().install()
+
+driver.get(URL)
