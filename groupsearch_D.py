235e6a6631d285f7f59ff32c996cb2a7a844efee
KDKftw <ooo.kadoun@gmail.com>
2022-03-16 11:20:57 +0100

    group search seperate fnc, HP_C redesign for ew

--- groupsearch_D.py
+++ groupsearch_D.py
@@ -4,6 +4,56 @@ from to_import import acceptConsent, URL_groupsearch, setUp, tearDown
 import unittest
 from selenium.webdriver.support import expected_conditions as EC
 
+def groupSearch_D(self, driver):
+    wait = WebDriverWait(self.driver, 150000)
+    wait.until(EC.visibility_of(driver.find_element_by_xpath("//*[@class='f_teaser-item']")))
+    teaserItems = driver.find_elements_by_xpath("//*[@class='f_teaser-item']")
+    try:
+        for WebElement in teaserItems:
+            ##print(len(teaserItems))
+            jdouvidet = WebElement.is_displayed()
+            ##print(jdouvidet)
+            if jdouvidet == True:
+                ##print(jdouvidet)
+                ##print(WebElement)
+                pass
+
+            else:
+                pass
+                ##print("Else")
+                ##emailfunciton
+
+    except NoSuchElementException:
+        pass
+        ##print("no such")
+        ##email fnction
+
+    assert teaserItems[0].is_displayed() == True
+
+    srlItems = driver.find_elements_by_xpath("//*[@class='f_searchResult'and not(@style='display: none;')]")
+    try:
+        for WebElement in srlItems:
+            ##print(len(srlItems))
+            jdouvidet = WebElement.is_displayed()
+            ##print(jdouvidet)
+            if jdouvidet == True:
+                ##print(jdouvidet)
+                ##print(WebElement)
+                pass
+
+            else:
+                pass
+                print("Else")
+
+
+
+    except NoSuchElementException:
+        pass
+        print("no such")
+
+    assert srlItems[0].is_displayed() == True
+
+
 class Test_Groupsearch_D(unittest.TestCase):
     def setUp(self):
         setUp(self)
@@ -13,53 +63,8 @@ class Test_Groupsearch_D(unittest.TestCase):
 
     def test_groupsearch_D(self):
         driver = self.driver
-        wait = WebDriverWait(self.driver, 150000)
         self.driver.get(URL_groupsearch)
         acceptConsent(self.driver)
         #teaserItems = driver.find_elements_by_xpath("//*[@class='f_teaser-item']")
-        wait.until(EC.visibility_of(driver.find_element_by_xpath("//*[@class='f_teaser-item']")))
-        teaserItems = driver.find_elements_by_xpath("//*[@class='f_teaser-item']")
-        try:
-            for WebElement in teaserItems:
-                ##print(len(teaserItems))
-                jdouvidet = WebElement.is_displayed()
-                ##print(jdouvidet)
-                if jdouvidet == True:
-                    ##print(jdouvidet)
-                    ##print(WebElement)
-                    pass
-
-                else:
-                    pass
-                    ##print("Else")
-                    ##emailfunciton
-
-        except NoSuchElementException:
-            pass
-            ##print("no such")
-            ##email fnction
-
-        assert teaserItems[0].is_displayed() == True
-
-        srlItems = driver.find_elements_by_xpath("//*[@class='f_searchResult'and not(@style='display: none;')]")
-        try:
-            for WebElement in srlItems:
-                ##print(len(srlItems))
-                jdouvidet = WebElement.is_displayed()
-                ##print(jdouvidet)
-                if jdouvidet == True:
-                    ##print(jdouvidet)
-                    ##print(WebElement)
-                    pass
-
-                else:
-                    pass
-                    print("Else")
-
-
-
-        except NoSuchElementException:
-            pass
-            print("no such")
-
-        assert srlItems[0].is_displayed() == True
+        self.driver.maximize_window()
+        groupSearch_D(self, driver)
