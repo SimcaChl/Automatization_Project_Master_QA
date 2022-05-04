86c91b006bcdd0a6dabdec56e6968ad1e5e9b37e
KDKftw <ooo.kadoun@gmail.com>
2022-05-04 17:14:57 +0200

    no message

--- starter_ET.py
+++ starter_ET.py
@@ -2,5 +2,5 @@ from starter_master_browserstack import runner_tests_generalized
 from ET_Automation_Local_Deploy_PyCharm.starter_local import *
 import unittest
 
-#while True:
-runner_tests_generalized(suite_ET_full, "ET")
+while True:
+    runner_tests_generalized(suite_ET_full, "ET")
--- starter_EW.py
+++ starter_EW.py
@@ -4,5 +4,5 @@ import unittest
 #runner_tests_generalized(EW_full_suite())
 
 #C:\Users\KDK\Desktop\Automatization_Project_Master\starter_FW.py
-
-runner_tests_generalized(suite_EW_full, "EW")
+while True:
+    runner_tests_generalized(suite_EW_full, "EW")
