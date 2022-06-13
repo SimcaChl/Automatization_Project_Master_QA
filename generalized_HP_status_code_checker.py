e0ec0fa786db2612eb64201145415d33e0c44283
Ondra <60475700+KDKftw@users.noreply.github.com>
2022-06-13 13:26:43 +0200

    hp chcker

---
+++ generalized_HP_status_code_checker.py
@@ -0,0 +1,88 @@
+import smtplib
+from email.mime.text import MIMEText
+import requests
+import time
+from to_import_secret_master import emailPass
+
+mujMail = 'ooo.kadoun@gmail.com'
+filipMail = 'filip.rytych@seznam.cz'
+FW_list_of_emails_to_notify = ["ooo.kadoun@gmail.com", "Filip.RYTYCH@fischer.cz"]
+
+def sendEmailv2(msg, recipient):
+  fromx = 'alertserverproblem@gmail.com'
+  to = recipient
+  msg = MIMEText(msg)
+  msg['Subject'] = "HP CHECK"
+  msg['From'] = fromx
+  msg['To'] = to
+
+  server = smtplib.SMTP('smtp.gmail.com:587')
+  server.starttls()
+  server.ehlo()
+  server.login("alertserverproblem@gmail.com", emailPass)
+  server.sendmail(fromx, to, msg.as_string())
+  server.quit()
+pocetChecku = 0
+timerToNotify = 0
+
+FW_start_of_URL = "https://fischer.web"
+EW_start_of_URL = "https://exim.web"
+def URL_maker(brand_start_of_URL, srweb_number):
+
+    final_URL = brand_start_of_URL+srweb_number+".dtweb.cz"
+    #print(final_URL)
+    return(final_URL)
+
+def generalized_URL_status_check(brand, email_list_to_notify):
+    timerToNotify = 0
+    pocetChecku = 0
+    while True:
+            cisloNodu=1
+
+            for i in range(3):
+                email_list_position = 0
+                try:
+                    response = requests.get(URL_maker(EW_start_of_URL, str(cisloNodu)), timeout=45)
+                    print(response)
+                    print(response.status_code)
+
+                except requests.exceptions.Timeout:
+                        msg = "FW HP, TIME OUT, SRWEB1"
+                        sendEmailv2(msg, mujMail)
+                        sendEmailv2(msg, filipMail)
+                        print("TIME OUT WEB1")
+                except requests.exceptions.ConnectionError:
+                        msg = "FW HP ConnectionError, SRWEB1"
+                        sendEmailv2(msg, mujMail)
+                        sendEmailv2(msg, filipMail)
+                except:
+                        pass
+
+                if response.status_code ==500:
+                    for _ in email_list_to_notify:
+                        msg = brand + " HomePage vrací status code 500 " + "SRWEB " + str(cisloNodu) + "  " + URL_maker(FW_start_of_URL, str(cisloNodu))
+                        sendEmailv2(msg, email_list_to_notify[email_list_position])
+                        email_list_position = email_list_position + 1
+
+                if response.status_code == 404:
+                    for _ in email_list_to_notify:
+                        msg = brand + " HomePage vrací status code 500 " + "SRWEB " + str(cisloNodu) + "  " + URL_maker(EW_start_of_URL, str(cisloNodu))
+                        sendEmailv2(msg, email_list_to_notify[email_list_position])
+                        email_list_position = email_list_position + 1
+
+                print(cisloNodu)
+                cisloNodu = cisloNodu + 1
+                time.sleep(10)
+
+            timerToNotify = timerToNotify + 1
+            pocetChecku = pocetChecku + 1
+            if timerToNotify == 1000:
+                msg = "check byl spusten 1000"
+                sendEmailv2(msg, mujMail)
+                timerToNotify = 0
+            print("CHECK NUMERO  " + str(pocetChecku))
+            print("CHECK NUMERO  " + str(pocetChecku))
+            print("CHECK NUMERO  " + str(pocetChecku))
+            time.sleep(50)
+
+generalized_URL_status_check("Fischer", FW_list_of_emails_to_notify)
