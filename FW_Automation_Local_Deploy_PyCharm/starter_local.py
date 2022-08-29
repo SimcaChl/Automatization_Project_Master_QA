1ef8ef907211a2298e96c8dcf0dc4f8eb2323214
Ondra <60475700+KDKftw@users.noreply.github.com>
2022-08-29 17:37:36 +0200

    updates

--- FW_Automation_Local_Deploy_PyCharm/starter_local.py
+++ FW_Automation_Local_Deploy_PyCharm/starter_local.py
@@ -84,7 +84,7 @@ if __name__ == '__main__':
     outfile = open("results.html", "w")
     web_brand = "FISCHER"
     version = "220803.2"
-    #runner_tests_generalized(suite_FW_full, web_brand, version, URL)
+    runner_tests_generalized(suite_FW_full, web_brand, version, URL)
 
-    runner_tests_generalized(SRL_suite_full, web_brand, version, URL)
+    #runner_tests_generalized(SRL_suite_full, web_brand, version, URL)
     #runner_tests_generalized(suite2, web_brand, version, URL)
--- FW_Automation_Local_Deploy_PyCharm/to_import.py
+++ FW_Automation_Local_Deploy_PyCharm/to_import.py
@@ -23,7 +23,8 @@ desired_cap = {
 }
 def setUp(self):
   #self.driver = webdriver.Remote(command_executor=comandExecutor,desired_capabilities=desired_cap)
-  self.driver = webdriver.Chrome(ChromeDriverManager().install())
+  #self.driver = webdriver.Chrome(ChromeDriverManager().install())
+  self.driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
   self.test_passed = False
 
 #URL = "https://www.fischer.cz/"
@@ -41,11 +42,11 @@ URL_detail = URL+"recko/rhodos/kalithea/amira?KEY=0xAC636C6ABB1245F78140DDD5CD0B
 ##atcom spaneslko
 #URL_SRL = URL + "vysledky-vyhledavani?ac1=2&d=1009|680|953|1108|592|611|610|612|1010|590|726|609|621|622|669|1086|1194|670|978|594|675|683&dd=2023-02-01&ka1=11&kc1=1&nn=7|8|9|10|11|12|13&rd=2023-02-26&to=4312|4305|2682|4308&tt=1"
 ##atcom egypt
-URL_SRL = URL + "vysledky-vyhledavani?ac1=2&d=653|819|724&dd=2023-02-01&ka1=4|14&kc1=2&nn=7|8|9|10|11|12|13&rd=2023-03-31&to=4312|4305|2682|4308&tt=1"
+#URL_SRL = URL + "vysledky-vyhledavani?ac1=2&d=653|819|724&dd=2023-02-01&ka1=4|14&kc1=2&nn=7|8|9|10|11|12|13&rd=2023-03-31&to=4312|4305|2682|4308&tt=1"
 
 #URL_SRL = URL+"vysledky-vyhledavani?d=826|1225|623|741|735|618|619|624|973|993|595|972|648|620|746|1126|1129|1124|1128|1059|1118|1119|1121|625|1127|1125|861|1115|1132|1120|709|711|1117|603|1116|1130|1131|614|1123|1093|1198|1114|1122&tt=1&to=4312|4305|2682|4308&dd=2022-07-01&rd=2022-08-31&nn=7|8|9|10|11|12|13&ka1=5&kc1=1&ac1=2"
 #URL_SRL = URL+"vysledky-vyhledavani?d=653|819|724&tt=1&to=4312|4305|2682|4308&dd=2022-07-01&rd=2022-08-31&nn=7|8|9|10|11|12|13&ac1=2"
-#URL_SRL = URL+"vysledky-vyhledavani?d=1009|680|953|1108|592|611|610|612|1010|590|726|609|621|622|669|1086|1194|670|978|594|675|683&tt=1&to=4312|4305|2682|4308&dd=2022-07-01&rd=2022-08-31&nn=7|8|9|10|11|12|13&ac1=2"
+URL_SRL = URL+"vysledky-vyhledavani?ac1=2&d=1009|680|953|1108|592|611|610|612|1010|590|726|609|621|622|669|1086|1194|670|978|594|675|683&dd=2022-09-01&ka1=8&kc1=1&nn=7|8|9|10|11|12|13&rd=2022-09-30&to=4312|4305|2682|4308&tt=1"
 #URL_SRL = URL+"vysledky-vyhledavani?d=1009|680|953|1108|592|611|610|612|1010|590|726|609|621|622|669|1086|1194|670|978|594|675|683&tt=1&to=4312|4305|2682|4308&dd=2022-07-01&rd=2022-08-31&nn=7|8|9|10|11|12|13&ka1=7&kc1=1&ac1=2"
 #URL_SRL = "https://fischer.web3.dtweb.cz/vysledky-vyhledavani?sortby=PriceTotal&d=1009|680|953|1108|592|611|610|612|1010|590|726|609|621|622|669|1086|1194|670|978|594|675|683&tt=1&to=4312|4305|2682|4308&dd=2022-07-01&rd=2022-08-31&nn=7|8|9|10|11|12|13&ka1=7&kc1=1&ac1=2"
 
