85e52fc6c455b7bf330e44150456db6125741561
Ondra <ooo.kadoun@gmail.com>
2022-09-19 15:56:13 +0200

    Update testing_file_2.py

--- testing_file_2.py
+++ testing_file_2.py
@@ -4,87 +4,21 @@ from webdriver_manager.chrome import ChromeDriverManager
 from FW_Automation_Local_Deploy_PyCharm.to_import import acceptConsent
 
 
-def price_sorter_expensive_cheap_assert(inputList, typeOfSort):
-    #print(inputList)
-    if typeOfSort == "cheap":
-        inputListSorted = inputList.copy()
-        inputListSorted.sort()
-        #inputListSorted = inputList  ##sorting second list low to high
-        #inputListSorted = inputList.sort()
-        #inputListSorted
-
-        if inputList == inputListSorted:  ##compare first list to second list, if is equal = good
-            print("Cheap sorter is OK")
-
-        else:
-            print("Cheap sorter is NOT OK")
-
-    if typeOfSort == "expensive":
-        inputListSorted = inputList.copy()
-        inputListSorted.sort(reverse=True)
-        #inputListSorted = inputList.sort(reverse=True)
-        if inputList == inputListSorted:
-            print("Expensive sorter is OK")
-        else:
-            print("Expensive sorter is NOT OK")
-
-    print("LIST FROM WEB:")
-    print(inputList)
-    print("CORRECTLY SORTED LIST")
-    print(inputListSorted)
-
-    assert inputList == inputListSorted
-
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
-URL = "https://www.fischer.cz/spanelsko/fuerteventura/morro-jable/blue-sea-jandia-luz?AC1=2&D=680|953|1108|592|611|610|612|590|726|609|621|1009|622|669|1086|1194|670|978|594|675|1010|683&DD=2023-02-19&DP=4312&DPR=FISCHER+ATCOM&DS=256&GIATA=32289&HID=1629&IC1=0&KC1=0&MNN=7&MT=6&NN=7&PID=FUE90003&RD=2023-02-26&TO=4312|4305|2682|4308&acm1=2&df=2023-02-01|2023-03-31&nnm=7|8|9|10|11|12|13&sortby=Departure&tom=4312|4305|2682|4308&tt=1&ttm=1#/terminy-a-ceny"
+URL = "https://www.eximtours.cz/poznavaci-zajezdy"
 driver.get(URL)
 time.sleep(1)
 driver.maximize_window()
 acceptConsent(driver)
 time.sleep(15)
+gridItemXpath = "//*[@class='f_tileGrid-item']/a"
+gridItemElements = driver.find_elements_by_xpath(gridItemXpath)
+#print(URL_poznavaciho_hotelu)
+linksToCheck_List = []
+pozice=0
+for _ in gridItemElements:
+    odkazLink = gridItemElements[pozice].get_attribute("href")
 
-boxTerminyXpath = "//*[@class='f_holder']"
-boxTerminyElement = driver.find_element_by_xpath(boxTerminyXpath)
-driver.execute_script("arguments[0].scrollIntoView();", boxTerminyElement)
-time.sleep(4)
-
-
-celkovaCenaSorterXpath = "//*[@class='f_termList-header-item f_termList-header-item--price']//*[@class='f_anchor f_icon f_icon_set--right f_icon_set--inheritColor']"
-celkovaCenaSorterElement = driver.find_element_by_xpath(celkovaCenaSorterXpath)
-##2x click = od nejrdazshi
-##1x click = od nejlevnejsiho
-
-celkovaCenaSorterElement.click()
-time.sleep(10)
-celkovaCenaSorterElement.click()
-time.sleep(10)
-
-##at this point kliknuto na sorter, need to take all of them and sort and compare lists / values
-
-##elemenet vypada jako "41 276 Kč"
-##odstranit menu na konci (parametr def by culture how long it is) + normalize space = should be int
-"38 764 Kč"
-
-
-pocetTerminuXpath = "//*[@class='f_termList-header-item']"
-pocetTerminuElements = driver.find_elements_by_xpath(pocetTerminuXpath)
-poziceTerminu = 0
-celkoveCenyList = []
-for _ in pocetTerminuElements:
-
-    celkoveCenaVterminechXpath = "//*[@class='f_termList-header-item f_termList-header-item--price']//*[@class='f_price pl-1 xlg:pl-0']"
-    celkoveCenaVterminechElements = driver.find_elements_by_xpath(celkoveCenaVterminechXpath)
-    kcIndex = 2
-    celkovaCenaVterminechINT = celkoveCenaVterminechElements[poziceTerminu].text[:-kcIndex].replace(" ", "")
-    celkovaCenaVterminechINT = int(celkovaCenaVterminechINT)
-    celkoveCenyList.append(celkovaCenaVterminechINT)
-    poziceTerminu = poziceTerminu+1
-print(celkoveCenyList)
-
-time.sleep(3)
-cheap = "cheap"
-price_sorter_expensive_cheap_assert(celkoveCenyList, cheap)
-
-
-
+    print(odkazLink)
+    pozice = pozice+1
