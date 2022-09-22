a4875d003765557ec234ca469c82c688c0b08e57
Ondra <60475700+KDKftw@users.noreply.github.com>
2022-09-22 16:06:22 +0200

    Update testing_file_4.py

--- testing_file_4.py
+++ testing_file_4.py
@@ -7,7 +7,7 @@ from FW_Automation_Local_Deploy_PyCharm.to_import import acceptConsent
 driver = webdriver.Chrome(ChromeDriverManager().install())
 #URL = "https://exim.stg.dtweb.cz/poznavaci-zajezdy"
 #URL = "https://www.fischer.cz/"
-URL = "https://fischer.web2.dtweb.cz/"
+URL = "https://fischer.web1.dtweb.cz/"
 driver.get(URL)
 time.sleep(1)
 driver.maximize_window()
