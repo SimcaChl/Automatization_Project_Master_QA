6d09362de8751cf50f0111d40fb968e23b1e3b3d
Ondra <60475700+KDKftw@users.noreply.github.com>
2022-10-26 12:43:35 +0200

    hp c updates

--- EW_Automation_Local_Deploy_PyCharm/HP_C.py
+++ EW_Automation_Local_Deploy_PyCharm/HP_C.py
@@ -16,8 +16,9 @@ HPvyhledatZajezdyButtonXpath = "//*[@class='f_filterMainSearch']//*[contains(tex
 HPkamPojedeteButtonXpath = "//*[contains(text(), 'Kam se chystáte?')]"
 HPzlutakReckoDestinaceXpath = "//*[@class='f_input-content'] //*[contains(text(), 'Řecko')]"
 HPzlutakPokracovatButtonXpath = "//*[contains(text(), 'Pokračovat')]"
-HPzlutakPokracovatButtonXpathStep2 = "//*[@class='f_filterHolder f_set--active'] //*[@class=('f_button-text f_icon f_icon--chevronRight f_icon_set--right')]"
-HPzlutakLetniPrazdninyXpath = "//*[contains(text(), 'Podzimní prázdniny')]"
+#HPzlutakPokracovatButtonXpathStep2 = "//*[@class='f_filterHolder f_set--active'] //*[@class=('f_button-text f_icon f_icon--chevronRight f_icon_set--right')]"
+HPzlutakPokracovatButtonXpathStep2 ="/html/body/header/div/div[2]/div/div/div/div[2]/div[2]/div[3]/div[2]/a/span"
+HPzlutakLetniPrazdninyXpath = "//*[contains(text(), 'Letní prázdniny 2023')]"
 HPzlutakPridatPokojXpath = "//*[contains(text(), 'přidat pokoj')]"
 HPzlutakObsazenost2plus1Xpath = "//*[contains(text(), 'Rodina 2+1')]"
 HPzlutakPotvrditAvyhledatXpath = "//*[@class='f_button f_button--common'] //*[contains(text(), 'Potvrdit a vyhledat')]"
@@ -62,8 +63,8 @@ class Test_HP_C(unittest.TestCase):
         wait.until(EC.visibility_of(self.driver.find_element_by_xpath(HPzlutakPokracovatButtonXpathStep2))).click()
 
         wait.until(EC.visibility_of(self.driver.find_element_by_xpath(HPzlutakLetniPrazdninyXpath))).click()
-        time.sleep(1.5)
-        wait.until(EC.visibility_of(self.driver.find_element_by_xpath(HPzlutakPokracovatButtonXpathStep2))).click()
+        time.sleep(2.5) ##+1
+        wait.until(EC.visibility_of(self.driver.find_element_by_xpath(HPzlutakPokracovatButtonXpath))).click()
         time.sleep(1.4)
 
         wait.until(EC.visibility_of(self.driver.find_element_by_xpath(HPzlutakObsazenost2plus1Xpath))).click()
@@ -114,31 +115,31 @@ class Test_HP_C(unittest.TestCase):
 
         self.test_passed = True
 
-    def test_HP_slider_click_detail_hotelu(self):
-        self.driver.get(URL)
-        wait = WebDriverWait(self.driver, 25)
-        self.driver.maximize_window()
-        time.sleep(
-            2.3)  ##this is to workaround accept consent since in maximizes and then selenium gets confused with clickin on the element
-        acceptConsent(self.driver)
-        # wait.until(EC.visibility_of(self.driver.find_element_by_xpath(HPnextArrowXpath))).click()
-        # time.sleep(10)
-        self.driver.implicitly_wait(100)
-        topNabidkaBigHotelCardXpath  = "//*[@class='page-widget js-ajaxPlaceholder--widget fshr-widget f_tileGrid-item f_tileGrid-item--double']"
-        topNabidkaBigHotelCardElement = self.driver.find_element_by_xpath(topNabidkaBigHotelCardXpath)
-
-        self.driver.execute_script("arguments[0].scrollIntoView();", topNabidkaBigHotelCardElement )
-        # self.driver.execute_script("arguments[0].scrollIntoView();", HPkartaHoteluSliderElement)
-        # action.move_to_element(HPkartaHoteluSliderElement).click().perform()
-        self.driver.implicitly_wait(100)
-        time.sleep(6)
-        topNabidkaBigHotelCardElement.click()
-        time.sleep(2)
-        curURL = self.driver.current_url
-
-        assert curURL != URL
-
-        self.test_passed = True
+    # def test_HP_slider_click_detail_hotelu(self):
+    #     self.driver.get(URL)
+    #     wait = WebDriverWait(self.driver, 25)
+    #     self.driver.maximize_window()
+    #     time.sleep(
+    #         2.3)  ##this is to workaround accept consent since in maximizes and then selenium gets confused with clickin on the element
+    #     acceptConsent(self.driver)
+    #     # wait.until(EC.visibility_of(self.driver.find_element_by_xpath(HPnextArrowXpath))).click()
+    #     # time.sleep(10)
+    #     self.driver.implicitly_wait(100)
+    #     topNabidkaBigHotelCardXpath  = "//*[@class='page-widget js-ajaxPlaceholder--widget fshr-widget f_tileGrid-item f_tileGrid-item--double']"
+    #     topNabidkaBigHotelCardElement = self.driver.find_element_by_xpath(topNabidkaBigHotelCardXpath)
+    #
+    #     self.driver.execute_script("arguments[0].scrollIntoView();", topNabidkaBigHotelCardElement )
+    #     # self.driver.execute_script("arguments[0].scrollIntoView();", HPkartaHoteluSliderElement)
+    #     # action.move_to_element(HPkartaHoteluSliderElement).click().perform()
+    #     self.driver.implicitly_wait(100)
+    #     time.sleep(6)
+    #     topNabidkaBigHotelCardElement.click()
+    #     time.sleep(2)
+    #     curURL = self.driver.current_url
+    #
+    #     assert curURL != URL
+    #
+    #     self.test_passed = True
 
     def test_HP_bannery_check(self):
         banner_check_public_prod_VS_deployed_web(self.driver, URL_prod_public, URL_deploying_web, banneryXpath_EW)
