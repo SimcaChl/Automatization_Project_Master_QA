1d637a5cbea80fbc1bf3fb69bfc59aa4c576a6db
Ondra <60475700+KDKftw@users.noreply.github.com>
2022-11-09 17:07:03 +0100

    dev

---
+++ EW_Automation_Local_Deploy_PyCharm/starter_local2.py
@@ -0,0 +1,97 @@
+from EW_Automation_Local_Deploy_PyCharm.CovidInfo_D import *
+from EW_Automation_Local_Deploy_PyCharm.pobocky import *
+from EW_Automation_Local_Deploy_PyCharm.Detail_D import *
+from EW_Automation_Local_Deploy_PyCharm.Detail_C import *
+from EW_Automation_Local_Deploy_PyCharm.DetskeKluby_D import *
+from EW_Automation_Local_Deploy_PyCharm.FM_D import *
+from EW_Automation_Local_Deploy_PyCharm.fulltext_C import *
+from EW_Automation_Local_Deploy_PyCharm.groupsearch_D import *
+from EW_Automation_Local_Deploy_PyCharm.HP_D import *
+from EW_Automation_Local_Deploy_PyCharm.LM_D import *
+from EW_Automation_Local_Deploy_PyCharm.poznavacky import *
+from EW_Automation_Local_Deploy_PyCharm.SDO_D import *
+from EW_Automation_Local_Deploy_PyCharm.SRL_C import *
+from EW_Automation_Local_Deploy_PyCharm.SRL_D import *
+#import HtmlTestRunner
+import HTMLTestRunner
+from EW_Automation_Local_Deploy_PyCharm.HP_C import *
+
+def suite_EW_full():
+    suite = unittest.TestSuite()
+    suite.addTest(TestCovidInfo_D('test_covidInfo_D'))
+    suite.addTest(TestDetailHotelu_D("test_detail_D"))
+    suite.addTest(TestDetailHotelu_C("test_detail_fotka"))
+    suite.addTest(TestDetailHotelu_C("test_detail_terminy_filtr_meal"))
+    suite.addTest(TestDetailHotelu_C("test_detail_terminy_filtr_airport"))
+    suite.addTest(TestDetskeKluby_D("test_kluby_D"))
+    suite.addTest(TestFM_D("test_FM_D"))
+    suite.addTest(Test_Fulltext_C("test_fulltext_naseptavac"))
+    suite.addTest(Test_Fulltext_C("test_fulltext_results_status_check"))
+    suite.addTest(Test_Groupsearch_D("test_groupsearch_D"))
+    suite.addTest(TestHP_D("test_homePage_D"))
+    suite.addTest(TestLM_D("test_lM_isDisplayed"))
+    suite.addTest(TestPobocky_D('test_pobocky_D'))
+    suite.addTest(TestPoznavacky_D('test_poznavacky_okruzni_D'))
+    suite.addTest(TestPoznavacky_D('test_poznavacky_vikendy_D'))
+    suite.addTest(TestPoznavacky_D('test_poznavacky_rodiny_D'))
+    suite.addTest(TestPoznavacky_D('test_poznavacky_zazitky_D'))
+    suite.addTest(TestSDO_D('test_SDO_D'))
+    suite.addTest(Test_SRL_C('test_SRL_sort_cheapest'))
+    suite.addTest(Test_SRL_C('test_SRL_sort_expensive'))
+    suite.addTest(Test_SRL_C('test_SRL_map'))
+    suite.addTest(Test_SRL_C('test_SRL_filtr_strava'))
+    suite.addTest(Test_SRL_C('test_srl_C'))
+    suite.addTest(TestSRL_D('test_SRL_D'))
+    suite.addTest(Test_HP_C('test_HP_zlutak_to_SRL'))
+    suite.addTest(Test_HP_C('test_HP_zlutak_to_groupsearch'))
+    suite.addTest(Test_HP_C('test_HP_nejlepsi_nabidky_vypis_btn_switch'))
+    #suite.addTest(Test_HP_C('test_HP_slider_click_detail_hotelu'))
+    suite.addTest(Test_HP_C('test_HP_bannery_check'))
+    ##############
+    ####################
+    ###########################
+    suite.addTest(TestPoznavacky_D('test_poznavacky_all_URL_check'))
+    suite.addTest(TestDetailHotelu_C("test_detail_price_sorter_terminy_cheap"))
+    suite.addTest(TestDetailHotelu_C("test_detail_price_sorter_terminy_expensive"))
+    return suite
+
+def suite2():
+    suite = unittest.TestSuite()
+
+    suite.addTest(TestFM_D("test_FM_D"))
+    suite.addTest(Test_SRL_C('test_SRL_filtr_strava'))
+    suite.addTest(Test_SRL_C('test_srl_C'))
+    suite.addTest(Test_HP_C('test_HP_bannery_check'))
+    return suite
+
+def suite3():
+    suite = unittest.TestSuite()
+
+    suite.addTest(Test_Fulltext_C("test_fulltext_naseptavac"))
+    suite.addTest(TestPobocky_D('test_pobocky_D'))
+    suite.addTest(Test_SRL_C('test_SRL_sort_cheapest'))
+    suite.addTest(Test_HP_C('test_HP_bannery_check'))
+
+    return suite
+
+def suite4():
+    suite = unittest.TestSuite()
+    suite.addTest(Test_HP_C('test_HP_bannery_check'))
+    return suite
+
+def SRL_suite_full():
+    suite = unittest.TestSuite()
+    suite.addTest(Test_SRL_C('test_SRL_sort_cheapest'))
+    suite.addTest(Test_SRL_C('test_SRL_sort_expensive'))
+    suite.addTest(Test_SRL_C('test_SRL_map'))
+    suite.addTest(Test_SRL_C('test_SRL_filtr_strava'))
+    suite.addTest(Test_SRL_C('test_srl_C'))
+    return suite
+from starter_master_browserstack import  runner_tests_generalized
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner()
+    outfile = open("results.html", "w")
+    web_brand = "EXIM"
+    runner_tests_generalized(suite_EW_full, web_brand, "hotifx", URL)
+    #runner_tests_generalized(SRL_suite_full, web_brand, "atcomcore deploy", URL)
+    #runner_tests_generalized(suite4, web_brand, "220718.1", URL)
