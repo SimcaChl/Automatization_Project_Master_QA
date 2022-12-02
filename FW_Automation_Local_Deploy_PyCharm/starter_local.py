5c1746d7a0d8b8b6f39fd471b0ed616685f874b0
Ondra <60475700+KDKftw@users.noreply.github.com>
2022-12-02 15:50:47 +0100

    Update starter_local.py

--- FW_Automation_Local_Deploy_PyCharm/starter_local.py
+++ FW_Automation_Local_Deploy_PyCharm/starter_local.py
@@ -87,7 +87,7 @@ if __name__ == '__main__':
     outfile = open("results.html", "w")
     web_brand = "FISCHER"
     version = "atcom online sputestni"
-    #runner_tests_generalized(suite_FW_full, web_brand, version, URL)
+    runner_tests_generalized(suite_FW_full, web_brand, version, URL)
 
-    runner_tests_generalized(SRL_suite_full, web_brand, version, URL)
+    #runner_tests_generalized(SRL_suite_full, web_brand, version, URL)
     #runner_tests_generalized(suite2, web_brand, version, URL)
