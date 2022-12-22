29e915a562be2120d2a706d05ef29a67ff989ace
Ondra <60475700+KDKftw@users.noreply.github.com>
2022-12-22 16:12:42 +0100

    URLS

--- EW_Automation_Local_Deploy_PyCharm/to_import.py
+++ EW_Automation_Local_Deploy_PyCharm/to_import.py
@@ -35,9 +35,9 @@ def setUp(self):
   self.test_passed = False
 
 
-URL = "https://www.eximtours.cz/"
+#URL = "https://www.eximtours.cz/"
 #URL = "https://exim.stg.dtweb.cz/"
-#URL = "https://exim.web11.dtweb.cz/"
+URL = "https://exim.web11.dtweb.cz/"
 URL_poznavacky = URL+"poznavaci-zajezdy"
 URL_poznavacky_vikendy = URL+"poznavaci-zajezdy#vikendy"
 URL_poznavacky_rodiny = URL+"poznavaci-zajezdy#rodiny"
