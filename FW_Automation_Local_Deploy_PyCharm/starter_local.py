448f0a18797d08177d0cb4aacf81d6c4b26425f0
Ondra <60475700+KDKftw@users.noreply.github.com>
2023-01-05 23:02:07 +0100

    ups

--- FW_Automation_Local_Deploy_PyCharm/starter_local.py
+++ FW_Automation_Local_Deploy_PyCharm/starter_local.py
@@ -46,7 +46,7 @@ def suite_FW_full():
     suite.addTest(TestSRL_D('test_SRL_D'))
     suite.addTest(Test_HP_C('test_HP_zlutak_to_SRL'))
     suite.addTest(Test_HP_C('test_HP_zlutak_to_groupsearch'))
-    #suite.addTest(Test_HP_C('test_HP_nejlepsi_nabidky_vypis_btn_switch'))
+    suite.addTest(Test_HP_C('test_HP_nejlepsi_nabidky_vypis_btn_switch'))
     suite.addTest(Test_HP_C('test_HP_slider_click_detail_hotelu'))
     suite.addTest(Test_HP_C('test_HP_bannery_check'))
     #############################
