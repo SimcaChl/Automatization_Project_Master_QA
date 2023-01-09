5c5edd18c629624e41ac490b6a81d9c96dedb888
Ondra <60475700+KDKftw@users.noreply.github.com>
2023-01-09 13:31:59 +0100

    airport filtr fix

--- KTGHU_Automation_Local_Deploy_PyCharm/Detail_C.py
+++ KTGHU_Automation_Local_Deploy_PyCharm/Detail_C.py
@@ -7,7 +7,7 @@ import unittest
 
 ##global
 from generalized_test_functions import generalized_Detail_terminyAceny_potvrdit_chooseFiltr_new_detail, \
-    generalized_list_string_sorter
+    generalized_list_string_sorter, generalized_detail_departure_check
 
 #terminyAcenyTabXpath = "//*[@id='terminyaceny-tab']"
 terminyAcenyTabXpath = "//*[@class='f_menu f_menu--inline f_menu--sticky']//*[contains(text(),'Időpontok és Árak')]"
@@ -218,123 +218,151 @@ class TestDetailHotelu_C(unittest.TestCase):
 
     def test_detail_terminy_filtr_airport(self):
 
-        def omlouvamese_paragraph(self, driver):
-            time.sleep(1)
-            try:
-                omlouvameParagraph = self.driver.find_element_by_xpath(
-                    "//*[@class='fshr-paragraph fshr-paragraph--centered']")
-                if omlouvameParagraph.is_displayed():
-                    return
-
-            except NoSuchElementException:
-                pass
-
-
+        self.driver.maximize_window()
         self.driver.get(URL_detail)
 
+        time.sleep(1)
         acceptConsent(self.driver)
 
-        time.sleep(1)
-        closeExponeaBanner(self.driver)
-        wait = WebDriverWait(self.driver, 150)
+        dopravaBrnoXpath = "//*[@class='f_filterHolder f_set--active']//*[@value='489']"
+        dopravaBoxXpath = "//*[@class='f_holder']//*[@class='f_button-content f_icon f_icon--plane']"
 
-        try:
-            generalDriverWaitImplicit(self.driver)
-            terminyCeny = self.driver.find_element_by_xpath("//*[@id='terminyaceny-tab']")
-            wait.until(EC.visibility_of(terminyCeny))
-            ##terminyCeny.click()
-            self.driver.execute_script("arguments[0].click();", terminyCeny)
-            time.sleep(0.5)
-            try:
-                generalDriverWaitImplicit(self.driver)
-                time.sleep(2)
-                potvrdit = self.driver.find_element_by_xpath("//*[@data-testid='popup-closeButton']")
-                ##wait.until(EC.visibility_of(potvrdit))
-                self.driver.execute_script("arguments[0].click();", potvrdit)
+        generalized_Detail_terminyAceny_potvrdit_chooseFiltr_new_detail(self.driver, terminyAcenyTabXpath,
+                                                                        dopravaBoxXpath, dopravaBrnoXpath, True)
+        time.sleep(4)
+        pocetZobrazenychTerminuXpath = "//*[@class='f_termList-header-item f_termList-header-item--dateRange']"
+        odletyTerminyXpath = "//*[@class='f_termList-header-item f_termList-header-item--transport']"
+        departureToCompareTo = "budapest"
 
-            except NoSuchElementException:
-                url = self.driver.current_url
-                msg = "Problem prepnuti na terminy a ceny na detailu hotelu,potvrdit,  NoSuchElementException " + url
-                sendEmail(msg)
+        time.sleep(5)
+        generalized_detail_departure_check(self.driver, pocetZobrazenychTerminuXpath, odletyTerminyXpath,
+                                           departureToCompareTo, True)
 
-        except NoSuchElementException:
-            url = self.driver.current_url
-            msg = "Problem prepnuti na terminy a ceny na detailu hotelu, NoSuchElementException " + url
-            sendEmail(msg)
-
-        try:
-            dopravaBox = self.driver.find_element_by_xpath(
-                "//*[@class='fshr-button-content fshr-icon fshr-icon--plane js-selector--travel']")
-            wait.until(EC.visibility_of(dopravaBox))
-            self.driver.execute_script("arguments[0].click();", dopravaBox)
-            try:
-                dopravaKosice = self.driver.find_element_by_xpath(
-                    "//*[@data-value='489']")  ##natvrdo brno, no list shenanigans
-                self.driver.execute_script("arguments[0].click();", dopravaKosice)
-
-                time.sleep(0.5)
-                try:
-
-                    self.driver.execute_script("arguments[0].click();",
-                                               dopravaBox)  ##workaround, proste klikne znova na doprava box aby se to propsalo, potvrdit
-                    ##button mi nejak blbnul
-                    ##driver.execute_script("arguments[0].click();",
-                    ##potvrditButtonBox)
-
-                except NoSuchElementException:
-                    url = self.driver.current_url
-                    msg = "potvrditButtonBox, potvrzeni dopravy na detailu hotelu problém, NoSuchElementException " + url
-                    sendEmail(msg)
-
-            except NoSuchElementException:
-                url = self.driver.current_url
-                msg = "dopravaBrno, zvolení dopravy na detailu hotelu problém, NoSuchElementException " + url
-                sendEmail(msg)
-
-        except NoSuchElementException:
-            url = self.driver.current_url
-            msg = "dopravaBox, zvolení dopravy na detailu hotelu problém, NoSuchElementException " + url
-            sendEmail(msg)
-
-        time.sleep(1)  ##cekacka na terminy load
-        omlouvamese_paragraph(self, self.driver)
-
-        try:
-            pocetZobrazenychTerminu = self.driver.find_elements_by_xpath(
-                "//*[@class='fshr-termins-table-item-header js-toggleSlide']")  ##locator jen na pocet odletu alokuje vic veci nez je actual terminu tak
-            ##pro
-            ##for
-            ##loop
-            ##pouziju
-            ##tohle
-            ##=
-            ##20
-        except NoSuchElementException:
-            url = self.driver.current_url
-            msg = "pocetZobrazenychTerminu, filtrovani dle letu detail hotelu, mozna jen nema odlety na X, NoSuchElementException " + url
-            sendEmail(msg)
-
-        try:
-            odletyTerminy = self.driver.find_elements_by_xpath(
-                "//*[@class='fshr-termin-departure-from']")  ##prvni locator je "odlet" takze zacnu na pozici jedna, vyloopuje se to podle
-            ##poctu terminu, should be ok
-        except NoSuchElementException:
-            url = self.driver.current_url
-            msg = "odletyTerminy, nejsou odlety na brno, most likely not a bad thing, NoSuchElementException " + url
-            sendEmail(msg)
-        y = 1
-        for _ in pocetZobrazenychTerminu:
-            assert odletyTerminy[y].text == "Budapest"
-            if odletyTerminy[y].text == "Budapest":  ##tady je nutny pricitat +2 protoze je tam 41 results (s tim ze jeden
-                ##je "odlet"), kazdy sudy cislo je mezera/blank space for some reason
-                ##print(odletyTerminy[y].text)
-                y = y + 2
-            else:
-                url = self.driver.current_url
-                ##print(odletyTerminy[y].text)
-                msg = "na detailu jsem vyfiltroval odlet na Budapest ale pry to nesedi říká python " + url
-                sendEmail(msg)
-                y = y + 2
+        time.sleep(0.2)
         self.test_passed = True
+
+
+        # def omlouvamese_paragraph(self, driver):
+        #     time.sleep(1)
+        #     try:
+        #         omlouvameParagraph = self.driver.find_element_by_xpath(
+        #             "//*[@class='fshr-paragraph fshr-paragraph--centered']")
+        #         if omlouvameParagraph.is_displayed():
+        #             return
+        #
+        #     except NoSuchElementException:
+        #         pass
+        #
+        #
+        # self.driver.get(URL_detail)
+        #
+        # acceptConsent(self.driver)
+        #
+        #
+
+        #
+        #
+        # time.sleep(1)
+        # closeExponeaBanner(self.driver)
+        # wait = WebDriverWait(self.driver, 150)
+        #
+        # try:
+        #     generalDriverWaitImplicit(self.driver)
+        #     terminyCeny = self.driver.find_element_by_xpath(terminyAcenyTabXpath)
+        #     wait.until(EC.visibility_of(terminyCeny))
+        #     ##terminyCeny.click()
+        #     self.driver.execute_script("arguments[0].click();", terminyCeny)
+        #     time.sleep(0.5)
+        #     try:
+        #         generalDriverWaitImplicit(self.driver)
+        #         time.sleep(2)
+        #         potvrdit = self.driver.find_element_by_xpath("//*[@data-testid='popup-closeButton']")
+        #         ##wait.until(EC.visibility_of(potvrdit))
+        #         self.driver.execute_script("arguments[0].click();", potvrdit)
+        #
+        #     except NoSuchElementException:
+        #         url = self.driver.current_url
+        #         msg = "Problem prepnuti na terminy a ceny na detailu hotelu,potvrdit,  NoSuchElementException " + url
+        #         sendEmail(msg)
+        #
+        # except NoSuchElementException:
+        #     url = self.driver.current_url
+        #     msg = "Problem prepnuti na terminy a ceny na detailu hotelu, NoSuchElementException " + url
+        #     sendEmail(msg)
+        #
+        # try:
+        #     dopravaBox = self.driver.find_element_by_xpath(
+        #         "//*[@class='fshr-button-content fshr-icon fshr-icon--plane js-selector--travel']")
+        #     wait.until(EC.visibility_of(dopravaBox))
+        #     self.driver.execute_script("arguments[0].click();", dopravaBox)
+        #     try:
+        #         dopravaKosice = self.driver.find_element_by_xpath(
+        #             "//*[@data-value='489']")  ##natvrdo brno, no list shenanigans
+        #         self.driver.execute_script("arguments[0].click();", dopravaKosice)
+        #
+        #         time.sleep(0.5)
+        #         try:
+        #
+        #             self.driver.execute_script("arguments[0].click();",
+        #                                        dopravaBox)  ##workaround, proste klikne znova na doprava box aby se to propsalo, potvrdit
+        #             ##button mi nejak blbnul
+        #             ##driver.execute_script("arguments[0].click();",
+        #             ##potvrditButtonBox)
+        #
+        #         except NoSuchElementException:
+        #             url = self.driver.current_url
+        #             msg = "potvrditButtonBox, potvrzeni dopravy na detailu hotelu problém, NoSuchElementException " + url
+        #             sendEmail(msg)
+        #
+        #     except NoSuchElementException:
+        #         url = self.driver.current_url
+        #         msg = "dopravaBrno, zvolení dopravy na detailu hotelu problém, NoSuchElementException " + url
+        #         sendEmail(msg)
+        #
+        # except NoSuchElementException:
+        #     url = self.driver.current_url
+        #     msg = "dopravaBox, zvolení dopravy na detailu hotelu problém, NoSuchElementException " + url
+        #     sendEmail(msg)
+        #
+        # time.sleep(1)  ##cekacka na terminy load
+        # omlouvamese_paragraph(self, self.driver)
+        #
+        # try:
+        #     pocetZobrazenychTerminu = self.driver.find_elements_by_xpath(
+        #         "//*[@class='fshr-termins-table-item-header js-toggleSlide']")  ##locator jen na pocet odletu alokuje vic veci nez je actual terminu tak
+        #     ##pro
+        #     ##for
+        #     ##loop
+        #     ##pouziju
+        #     ##tohle
+        #     ##=
+        #     ##20
+        # except NoSuchElementException:
+        #     url = self.driver.current_url
+        #     msg = "pocetZobrazenychTerminu, filtrovani dle letu detail hotelu, mozna jen nema odlety na X, NoSuchElementException " + url
+        #     sendEmail(msg)
+        #
+        # try:
+        #     odletyTerminy = self.driver.find_elements_by_xpath(
+        #         "//*[@class='fshr-termin-departure-from']")  ##prvni locator je "odlet" takze zacnu na pozici jedna, vyloopuje se to podle
+        #     ##poctu terminu, should be ok
+        # except NoSuchElementException:
+        #     url = self.driver.current_url
+        #     msg = "odletyTerminy, nejsou odlety na brno, most likely not a bad thing, NoSuchElementException " + url
+        #     sendEmail(msg)
+        # y = 1
+        # for _ in pocetZobrazenychTerminu:
+        #     assert odletyTerminy[y].text == "Budapest"
+        #     if odletyTerminy[y].text == "Budapest":  ##tady je nutny pricitat +2 protoze je tam 41 results (s tim ze jeden
+        #         ##je "odlet"), kazdy sudy cislo je mezera/blank space for some reason
+        #         ##print(odletyTerminy[y].text)
+        #         y = y + 2
+        #     else:
+        #         url = self.driver.current_url
+        #         ##print(odletyTerminy[y].text)
+        #         msg = "na detailu jsem vyfiltroval odlet na Budapest ale pry to nesedi říká python " + url
+        #         sendEmail(msg)
+        #         y = y + 2
+        # self.test_passed = True
 
 
