20c2122b58abaa954e9e285229cdd5f018b31c1e
Ondra <ooo.kadoun@gmail.com>
2023-02-03 15:40:39 +0100

    Update pobocky_srandy.py

--- pobocky_srandy.py
+++ pobocky_srandy.py
@@ -13,32 +13,6 @@ driver.maximize_window()
 acceptConsent(driver)
 time.sleep(15)
 
-brnoAnchorOblibeneVolbyXpath = "//*[@class='f_anchor'and contains(text(), 'Brno')]"
-brnoAnchorOblibeneVolbyElement = driver.find_element_by_xpath(brnoAnchorOblibeneVolbyXpath)
-brnoAnchorOblibeneVolbyElement.click()
-
-time.sleep(2)
-
-pobockaBoxXpath = "//*[@data-branch-id='262']"
-pobockaBoxElement = driver.find_element_by_xpath(pobockaBoxXpath)
-pobockaBoxElement.click()
-
-detailPobockyXpath = pobockaBoxXpath + "//*[contains(text(), 'Detail pobočky')]"
-detailPobockyElement = driver.find_element_by_xpath(detailPobockyXpath)
-driver.execute_script("arguments[0].scrollIntoView();", detailPobockyElement)
-detailPobockyElement.click()
-
-objednatSchuzkuBtnXpath = "//*[@class='f_button f_button--important js-popupWindow--show js-gtm-eventClick']"
-objednatSchuzkuBtnElement = driver.find_element_by_xpath(objednatSchuzkuBtnXpath)
-objednatSchuzkuBtnElement.click()
-
-time.sleep(2)
-
-popUpObjednavkaNavstevyXpath = "//*[@class='fshr-popupWindow fshr-popupWindow--centered js-form js-popupWindow fshr-icon fshr-icon--man js-sendByAjax js-gtm-trackGoal']"
-popUpObjednavkaNavstevyElement = driver.find_element_by_xpath(popUpObjednavkaNavstevyXpath)
-print("Popup formulář je zobrazený:    " )
-print(popUpObjednavkaNavstevyElement.is_displayed())
-
 
 
 start = "//*[@data-branch-id='248']"
