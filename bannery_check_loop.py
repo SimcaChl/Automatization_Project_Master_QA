6846760b47c48be363663eaaf384796ecc40959f
Ondra <ooo.kadoun@gmail.com>
2023-02-08 17:02:09 +0100

    Update bannery_check_loop.py

--- bannery_check_loop.py
+++ bannery_check_loop.py
@@ -22,63 +22,66 @@ def sendEmailv2(msg, recipient):
   server.sendmail(fromx, to, msg.as_string())
   server.quit()
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-#URL = "https://exim.stg.dtweb.cz/poznavaci-zajezdy"
-URL = "https://www.fischer.cz"
-driver.get(URL)
-time.sleep(1)
-driver.maximize_window()
-acceptConsent(driver)
-time.sleep(15)
-banneryXpath_FW = "//*[@class='f_teaser-item']/a"
-banneryAll = driver.find_elements_by_xpath(banneryXpath_FW)
-SRL_H1textPocetNalezenychZajezduXpath = "//h1"
-x = 0
-pocetNalezenychZajezduElementList_PROD = []
-bannerLinksList_PROD = []
-for _ in banneryAll:
-    bannerHref = banneryAll[x].get_attribute("href")
-    # print(bannerHref)
-    x = x + 1
-    # print(x)
-    driver.execute_script("window.open("");")
-    driver.switch_to.window(driver.window_handles[1])
-    driver.get(bannerHref)
-    # time.sleep(10)
-    try:
-        pocetNalezenychZajezduElement_PROD = driver.find_element_by_xpath(
-            SRL_H1textPocetNalezenychZajezduXpath).text.lower()
-        pocetNalezenychZajezduElementList_PROD.append(pocetNalezenychZajezduElement_PROD)
-        bannerLinks_PROD = driver.current_url
-        bannerLinksList_PROD.append(bannerLinks_PROD)
-        # print (bannerLinks_PROD)
-    except NoSuchElementException:
-        pass
-    driver.close()
-    driver.switch_to.window(driver.window_handles[0])
-    time.sleep(0.5)
 
-print( pocetNalezenychZajezduElementList_PROD)
-print(bannerLinksList_PROD)
+while True:
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    URL = "https://www.fischer.cz"
+    driver.get(URL)
+    time.sleep(1)
+    driver.maximize_window()
+    acceptConsent(driver)
+    time.sleep(15)
+    banneryXpath_FW = "//*[@class='f_teaser-item']/a"
+    banneryAll = driver.find_elements_by_xpath(banneryXpath_FW)
+    SRL_H1textPocetNalezenychZajezduXpath = "//h1"
+    x = 0
+    pocetNalezenychZajezduElementList_PROD = []
+    bannerLinksList_PROD = []
+    for _ in banneryAll:
+        bannerHref = banneryAll[x].get_attribute("href")
+        # print(bannerHref)
+        x = x + 1
+        # print(x)
+        driver.execute_script("window.open("");")
+        driver.switch_to.window(driver.window_handles[1])
+        driver.get(bannerHref)
+        # time.sleep(10)
+        try:
+            pocetNalezenychZajezduElement_PROD = driver.find_element_by_xpath(
+                SRL_H1textPocetNalezenychZajezduXpath).text.lower()
+            pocetNalezenychZajezduElementList_PROD.append(pocetNalezenychZajezduElement_PROD)
+            bannerLinks_PROD = driver.current_url
+            bannerLinksList_PROD.append(bannerLinks_PROD)
+            # print (bannerLinks_PROD)
+        except NoSuchElementException:
+            pass
+        driver.close()
+        driver.switch_to.window(driver.window_handles[0])
+        time.sleep(0.5)
 
-nenasliCoJsteHledaliString = "Nenašli jste, co jste hledali?"
+    print( pocetNalezenychZajezduElementList_PROD)
+    print(bannerLinksList_PROD)
 
-
-y = 0
-for _ in pocetNalezenychZajezduElementList_PROD:
-    if pocetNalezenychZajezduElementList_PROD[y] == nenasliCoJsteHledaliString:
-        print(pocetNalezenychZajezduElementList_PROD[y])
-        print("spatny")
-        sendEmailv2(bannerLinksList_PROD[y] , "ondrej.kadoun@fischer.cz")
-
-    else:
-        print(pocetNalezenychZajezduElementList_PROD[y])
-        print("OK")
-
-    y = y + 1
-
-sendEmailv2("executed FW CHECK HP bannery", "ondrej.kadoun@fischer.cz")
-    #sendEmailv2(bannerLinksList_PROD[y], "ondrej.kadoun@fischer.cz")
+    nenasliCoJsteHledaliString = "Nenašli jste, co jste hledali?"
 
 
-    #print(y)
+    y = 0
+    for _ in pocetNalezenychZajezduElementList_PROD:
+        if pocetNalezenychZajezduElementList_PROD[y] == nenasliCoJsteHledaliString:
+            print(pocetNalezenychZajezduElementList_PROD[y])
+            print("spatny")
+            sendEmailv2(bannerLinksList_PROD[y] , "ondrej.kadoun@fischer.cz")
+
+        else:
+            print(pocetNalezenychZajezduElementList_PROD[y])
+            print("OK")
+
+        y = y + 1
+
+    sendEmailv2("executed FW CHECK HP bannery", "ondrej.kadoun@fischer.cz")
+        #sendEmailv2(bannerLinksList_PROD[y], "ondrej.kadoun@fischer.cz")
+
+
+        #print(y)
+
+    time.sleep(3600)
