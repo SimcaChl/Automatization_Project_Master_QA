a90f799e3ff58ed9722f03aeaf65c6b30f693a66
Ondra <ooo.kadoun@gmail.com>
2023-02-08 14:41:59 +0100

    asd

--- bannery_check_loop.py
+++ bannery_check_loop.py
@@ -1,23 +1,15 @@
 import time
-import requests
+
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from FW_Automation_Local_Deploy_PyCharm.to_import import acceptConsent
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 #URL = "https://exim.stg.dtweb.cz/poznavaci-zajezdy"
-URL = "https://www.fischer.cz/kontakty/seznam-pobocek"
+URL = "https://www.fischer.cz"
 driver.get(URL)
 time.sleep(1)
 driver.maximize_window()
 acceptConsent(driver)
 time.sleep(15)
-
-
-
-start = "//*[@data-branch-id='248']"
-end = "//*[@data-branch-id='493']"
-
-## https://webadmin-shared.stg.dtweb.cz/OfficeBranchAdmin?fragmentGuid=00000000-0000-0000-0000-000000000000
-
 
